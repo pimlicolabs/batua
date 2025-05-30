89096e974420b51fcc46daf75cbd725a3015bc51
Garvit Khatri <garvitdelhi@gmail.com>
2025-05-30 16:38:48 +0100

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,62 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+    outputFileTracingIncludes: {
+        registry: ["./registry/**/*"]
+    },
+
     async headers() {
         return [
             {
-                source: "/iframe", // Or '/:path*' if you want to apply to all paths
+                // apply to every path
+                source: "/:path*",
                 headers: [
                     {
                         key: "X-Frame-Options",
-                        value: "" // Or 'ALLOW-FROM https://your-specific-domain.com' or remove the header entirely by setting value to ''
+                        value: "ALLOWALL" // allows the page to be framed anywhere
                     }
+                    // — OPTIONAL, more modern and flexible —
+                    // {
+                    //   key: 'Content-Security-Policy',
+                    //   value: "frame-ancestors *",  // lets any site embed you
+                    // },
                 ]
             }
         ]
-    },
-    /* config options here */
-    reactStrictMode: true,
-    typescript: {
-        ignoreBuildErrors: true
-    },
-    experimental: {
-        ppr: "incremental",
-        typedRoutes: true
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "avatar.vercel.sh"
-            },
-            {
-                protocol: "https",
-                hostname: "images.unsplash.com"
-            },
-            {
-                protocol: "https",
-                hostname: "s3.amazonaws.com"
-            },
-            {
-                protocol: "https",
-                hostname: "lh3.googleusercontent.com"
-            }
-        ]
-    },
-    env: {
-        NEXT_PUBLIC_PIMLICO_API_KEY: process.env.NEXT_PUBLIC_PIMLICO_API_KEY,
-        NEXT_PUBLIC_SPONSORSHIP_POLICY_ID:
-            process.env.NEXT_PUBLIC_SPONSORSHIP_POLICY_ID
-    },
-    eslint: {
-        ignoreDuringBuilds: true
-    },
-    compiler: {
-        styledComponents: true,
-        paths: {
-            "@/*": ["./*"]
-        }
-    },
-    transpilePackages: ["@uniswap/widgets", "@uniswap/conedison"]
+    }
 }
 
 export default nextConfig
